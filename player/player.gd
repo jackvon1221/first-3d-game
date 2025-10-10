8157f9d055d56c82d77a0849c558699be350259d
Jack VonAchen <jackvonachen@gmail.com>
2025-10-10 18:16:12 -0500

    update

--- player/player.gd
+++ player/player.gd
@@ -38,7 +38,7 @@ func _physics_process(delta: float) -> void:
 		
 	move_and_slide()
 	
-	if Input.is_action_just_pressed("shoot"):
+	if Input.is_action_just_pressed("shoot") and %Timer.is_stopped():
 		shoot_bullet()
 	
 func shoot_bullet():
@@ -47,3 +47,4 @@ func shoot_bullet():
 	%Marker3D.add_child(new_bullet)
 	
 	new_bullet.global_transform = %Marker3D.global_transform
+	%Timer.start()
