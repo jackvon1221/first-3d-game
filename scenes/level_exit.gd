4d7e68ece600b0939af93738e6816f70a7792c36
Jack VonAchen <jackvonachen@gmail.com>
2026-01-15 19:46:03 -0600

    level 2 created

--- scenes/level_exit.gd
+++ scenes/level_exit.gd
@@ -1,5 +1,15 @@
 extends Area3D
 
+@onready var exit_sound = $ExitSound
+var triggered := false
+
 func _on_body_entered(body):
-	if body.name == "Player":
-		get_tree().change_scene_to_file("res://Level-02.tscn")
+	if triggered:
+		return
+	if body.is_in_group("player"):
+		triggered = true
+		if exit_sound:
+			exit_sound.play()
+		# Give the sound time to play
+		await get_tree().create_timer(0.9).timeout
+		get_tree().change_scene_to_file("res://scenes/Level-02.tscn")
