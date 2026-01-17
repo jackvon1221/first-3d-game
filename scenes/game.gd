67eb556970a06c42b90357bf035e110207bafe2f
Jack VonAchen <jackvonachen@gmail.com>
2026-01-16 19:29:10 -0600

    progress

--- scenes/game.gd
+++ scenes/game.gd
@@ -1,28 +1,38 @@
 extends Node3D
 
-var player_score = 0
 @onready var score_label: Label = %Label
 @onready var ten_point_sound: AudioStreamPlayer = $TenPointSound
 @onready var diedsound: AudioStreamPlayer = %diedsound
 
 
-func add_point_from_pickup():
-	player_score += 1
-	score_label.text = "SQUIBBOS: " + str(player_score)
+func _ready():
+	# Make sure the label shows the carried-over score
+	update_score_label()
 
-		
+
+func add_point_from_pickup():
+	GameManager.score += 1
+	update_score_label()
+	if GameManager.score % 10 == 0:
+		ten_point_sound.play()
+
+
+func update_score_label():
+	score_label.text = "SQUIBBOS: " + str(GameManager.score)
+
+
 func do_poof(mob_global_position):
 	const SMOKE_PUFF = preload("uid://cjk3frr43yesb")
 	var poof = SMOKE_PUFF.instantiate()
 	add_child(poof)
 	poof.global_position = mob_global_position
-	
+
+
 func _on_mob_spawned(mob: Node3D):
 	if mob.has_signal("died"):
 		mob.died.connect(func on_mob_died():
 			do_poof(mob.global_position)
-			)
-		do_poof(mob.global_position)
+		)
 
 
 func _on_killplane_body_entered(body: Node3D) -> void:
@@ -30,6 +40,6 @@ func _on_killplane_body_entered(body: Node3D) -> void:
 		diedsound.play()
 		await get_tree().create_timer(3.0).timeout
 		get_tree().reload_current_scene()
-		
-	elif "zombie" in str(body.name):  # Changed this line
+
+	elif "zombie" in str(body.name):
 		body.queue_free()
---
+++ scenes/horizontalfrontback.gd
@@ -0,0 +1,18 @@
+extends AnimatableBody3D
+
+@export var speed := 3.0
+@export var distance := 7.0
+
+var start_pos: Vector3
+var direction := 1.0
+
+func _ready():
+	start_pos = global_position
+
+func _physics_process(delta):
+	var offset = global_position.x - start_pos.x
+
+	if abs(offset) >= distance:
+		direction *= -1.0
+
+	global_position.x += direction * speed * delta
